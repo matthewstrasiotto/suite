9913c60aba45c37c5e69c75bcfb415c6c814b488
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-10-05 13:29:26 +1100

    Update vagrantfile to use latest ubuntu, and update vagrant bootstrap script

--- Vagrantfile
+++ Vagrantfile
@@ -9,8 +9,8 @@ ip = '192.168.70.70'
 
 VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "trusty-amd64"
-  config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.box = "ubuntu/focal64"
+  # config.vm.box_url = "http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
   config.vm.network :private_network, ip: ip
   if Vagrant.has_plugin? 'vagrant-hostmanager'
     config.hostmanager.enabled = true
--- vagrant.sh
+++ vagrant.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -e
 
+
+PIP_URL=https://bootstrap.pypa.io/pip/3.4/get-pip.py
+PIP_URL=https://bootstrap.pypa.io/get-pip.py
 # This file is used by the Vagrantfile to set up the dev environment
 
 sudo apt-get update
 
 sudo apt-get install --yes --force-yes zip unzip python python-dev \
+    python3-dev \
     build-essential curl redis-server python-setuptools
-easy_install -U pip
+easy_install -U pip || curl "$PIP_URL" -o get-pip.py && python3 get-pip.py
+rm get-pip.py
 
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password bacon'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password bacon'
 apt-get update --yes --force-yes
-apt-get install --yes --force-yes python-software-properties python-mysqldb mysql-server libmysqlclient-dev \
-    libffi-dev libssl-dev
+
+apt-get install --yes --force-yes python3-software-properties \
+  python3-mysqldb mysql-server \
+  libmysqlclient-dev default-libmysqlclient-dev \
+  libffi-dev libssl-dev
 
 printf "[client]\nuser = root\npassword = bacon" >> ~/.my.cnf
 
@@ -21,11 +29,18 @@ service mysql restart
 mysql -e "create database dev;"
 
 
-curl -sL https://deb.nodesource.com/setup | bash -
+NODE_SCRIPT_URL="https://deb.nodesource.com/setup_14.x"
+
+# For now, we need to install ca-certificates for node install to work
+# https://github.com/nodesource/distributions/issues/1266
+sudo apt-get --yes --force-yes install ca-certificates
+curl -sL "$NODE_SCRIPT_URL" | bash -
+apt-get update --yes --force-yes
+
 sudo apt-get --yes --force-yes install nodejs
 
 # let's get some docker going too
-curl -sSL https://get.docker.com/ | sh
+hash docker || curl -sSL https://get.docker.com/ | sh
 
 # have to do some weird shit to get nodejs to work without sudo -.-
 su vagrant -l -c 'npm config set prefix ~/npm'
@@ -42,6 +57,10 @@ echo "source /vagrant/vagrant-venv/bin/activate" >> /home/vagrant/.bashrc
 echo '* * * * * root curl --user staffjoydev: http://suite.local/api/v2/internal/cron/' >> /etc/crontab
 
 cd /vagrant/ && rm -rf vagrant-venv && virtualenv vagrant-venv
-source /vagrant/vagrant-venv/bin/activate && export PATH="$PATH:$HOME/npm/bin:/home/vagrant/node_modules/.bin:$PATH" && export ENV="dev" && cd /vagrant/ && make build && make dev-requirements && make db-deploy
+source /vagrant/vagrant-venv/bin/activate && export PATH="$PATH:$HOME/npm/bin:/home/vagrant/node_modules/.bin:$PATH" && \
+  export ENV="dev" && cd /vagrant/ && \
+  make build && \
+  #make dev-requirements && \
+  make db-deploy
 python /vagrant/dev-sudo.py
 
