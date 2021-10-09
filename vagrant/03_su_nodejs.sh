30daf33f8817af6f589eb876275e03ecd4940a03
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-10-09 19:24:03 +1100

    fixup! remove sudo from a script that is run by root

--- vagrant/03_su_nodejs.sh
+++ vagrant/03_su_nodejs.sh
@@ -4,9 +4,9 @@ NODE_SCRIPT_URL="https://deb.nodesource.com/setup_14.x"
 
 # For now, we need to install ca-certificates for node install to work
 # https://github.com/nodesource/distributions/issues/1266
-sudo apt-get --yes --force-yes install ca-certificates
+apt-get --yes --force-yes install ca-certificates
 curl -sL "$NODE_SCRIPT_URL" | bash -
 apt-get update --yes --force-yes
 
-sudo apt-get --yes --force-yes install nodejs
+apt-get --yes --force-yes install nodejs
 
