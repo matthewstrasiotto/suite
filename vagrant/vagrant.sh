5e7534e5f92264c22259dc4c5e71c7a8efa6b34a
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-10-09 17:03:44 +1100

    Vagrantfile: Slight refactor vagrant.sh

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-
 cd /vagrant/ && rm -rf vagrant-venv && virtualenv vagrant-venv
 
 rm -rf /vagrant-venv && virtualenv /vagrant-venv
 
-source /vagrant-venv/bin/activate && export PATH="$PATH:$HOME/npm/bin:/home/vagrant/node_modules/.bin:$PATH" && \
-  export ENV="dev" && cd /vagrant/ && \
-  make build && \
-  #make dev-requirements && \
-  make db-deploy
+source /vagrant-venv/bin/activate \
+  && export PATH="$PATH:$HOME/npm/bin:/home/vagrant/node_modules/.bin:$PATH" \
+  && export ENV="dev" \
+  && cd /vagrant/ \
+  && make build  \
+  # && make dev-requirements \
+  && make db-deploy
 python /vagrant/dev-sudo.py
 
